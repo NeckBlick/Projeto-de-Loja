3cd0dfc70c7a12aadfa2e9b9fce460c862db2d12
NeckBlick <nickmestre200@gmail.com>
2022-12-02 19:49:01 -0300

    Front perfil, Tela incial e Back editar e produtos

--- back-end/Models/product.js
+++ back-end/Models/product.js
@@ -1,10 +1,13 @@
 const mongoose = require('mongoose')
-const Schema = mongoose.Schema
 
 const product = mongoose.model("product", {
-    titulo: String,
-    descricao: String,
-    preco: String
+    image:{
+        data: Buffer,
+        contentType: String
+    },
+    title: String,
+    description: String,
+    price: String,
 })
 
 module.exports = product
--- back-end/server.js
+++ back-end/server.js
@@ -4,27 +4,62 @@ const cors = require('cors')
 const mongoose = require('mongoose')
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
+const multer  = require('multer')
+const upload = multer({ dest: 'uploads/' })
 
 const app = express()
 app.use(express.json())
 app.use(cors())
 const modelUser =  require('./Models/user')
+const modelProduct =  require('./Models/product')
 
-// Usuários
-app.get("/users" , async (req, res) => {
-    // Ver se existe o usuário
-    const user = await modelUser.find()
-    res.status(200).json({user})
+// Buscar produtos
+app.get("/products" , async (req, res) => {
+
+    const product = await modelProduct.find()
+    res.status(200).json({product})
 })
-// Usuário pelo id
-app.get("/user/:id" , checkToken , async (req, res) => {
-    const id = req.params.id
-    // Ver se existe o usuário
-    const user = await modelUser.findById(id)
-    if(!user){
-        return res.status(200).json({msg: "Usuario não encontrado!"})
+
+// Register product
+app.post("/product/register", async (req, res) => {
+    const image = req.file
+    const { title, description, price} = req.body
+    // Validação
+    if(!title){
+        return res.status(422).json({msg: "O titulo é obrigatório!"})
     }
-    res.status(200).json({user})
+    if(!description){
+        return res.status(422).json({msg: "A descrição é obrigatória!"})
+    }
+    if(!price){
+        return res.status(422).json({msg: "O preço é obrigatório!"})
+    }
+    // if(!image){
+    //     return res.status(422).json({msg: "A imagem é obrigatória!"})
+    // }
+    // Checar se existe o produto
+    const productExist = await modelProduct.findOne({title: title, description: description})
+    if(productExist){
+        return res.status(422).json({msg: "Esse produto já está cadastrado!"})
+    }
+
+    // Criar o usuário
+    const product = new modelProduct({
+        title,
+        description,
+        price, 
+        image
+    })
+
+    try{
+
+        await product.save()
+        res.status(201).json({msg: "Produto cadastrado com sucesso!"})
+
+    }catch(err){
+        console.log(err)
+        res.status(500).json({msg: "Houve um erro no servidor, tente novamente mais tarde!"})
+    }
 })
 
 function checkToken(req, res, next){
@@ -45,6 +80,61 @@ function checkToken(req, res, next){
         res.status(400).json({msg: "Token invalido!"})
     }
 }
+// Usuários
+app.get("/users" , async (req, res) => {
+
+    // Ver se existe o usuário
+    const user = await modelUser.find()
+    res.status(200).json({user})
+})
+
+// Usuário pelo id
+app.get("/user/:id" , checkToken , async (req, res) => {
+    const id = req.params.id
+    // Ver se existe o usuário
+    const user = await modelUser.findById(id)
+    if(!user){
+        return res.status(200).json({msg: "Usuario não encontrado!"})
+    }
+    res.status(200).json({user})
+})
+
+// Editar usuário 
+app.put("/user/:id" , checkToken , async (req, res) => {
+    const id = req.params.id
+    const { nome, email, senha, confirmsenha } = req.body
+    if(!nome){
+        return res.status(422).json({msg: "O nome é obrigatorio!"})
+    }
+    if(!email){
+        return res.status(422).json({msg: "O email é obrigatorio!"})
+    }
+    if(!senha){
+        return res.status(422).json({msg: "A senha é obrigatorio!"})
+    }
+    if(senha != confirmsenha){
+        return res.status(422).json({msg: "As senhas são diferentes!"})
+    }
+
+    const salt = await bcrypt.genSalt(12)
+    const hashSenha = await bcrypt.hash(senha, salt)
+    try{
+
+        await modelUser.findByIdAndUpdate({_id: id},{
+            nome: nome,
+            email: email,
+            senha: hashSenha
+        })
+        res.status(201).json({msg: "Usuario editado com sucesso!"})
+
+    }catch(err){
+        console.log(err)
+        res.status(500).json({msg: "Houve um erro no servidor, tente novamente mais tarde!"})
+    }
+    
+})
+
+
 
 // Register Route
 app.post("/auth/register", async (req, res) => {
